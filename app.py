065155364af18d02b42c468dc8ac9e871abeecb4
Bill Wang <bill@gameology.space>
2024-08-27 14:59:42 -0700

    chore: Update app.py to use environment variable for port number

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def ping():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
