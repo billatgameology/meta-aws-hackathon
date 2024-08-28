9d5ddeb84b8d75ff3123bde211c3e1331742e1c3
Bill Wang <bill@gameology.space>
2024-08-27 18:47:26 -0700

    chore: Remove unused bedrocktest.py module and update dependencies

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, request, jsonify
-#from bedrocktest import get_style_options
 
 
 app = Flask(__name__)
@@ -8,13 +7,6 @@ app = Flask(__name__)
 @app.route('/ping', methods=['GET'])
 def ping():
     return 'Hello, World!'
-
-# @app.route('/styles', methods=['POST'])
-# def styles():
-#     data = request.json
-#     customer_input = data.get('customer_input', '')
-#     styles = get_style_options(customer_input)
-#     return jsonify(styles)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))  
